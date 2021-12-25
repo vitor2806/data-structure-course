189e4f07fe8856754341219d223936ba8f70628e
Vitor Rafael <vitor.45678.rafael@gmail.com>
2021-12-25 11:50:02 -0300

    add array.splice() structure

--- index.js
+++ index.js
@@ -27,3 +27,11 @@ for (let hero of heroes) {
 //Bad perfomance function, it searches the entire array looking for the specified value, so if theres a big array it will cause throttling
 const thrall = heroes.find((hero) => hero === 'Thrall');
 console.log(thrall);
+
+//Also bad for perfomance solution, it will delete array elements between an interval then reallocate all others elements into new positions
+heroes.splice(11, 11);
+console.clear();
+
+for (let hero of heroes) {
+	console.log(hero);
+}
